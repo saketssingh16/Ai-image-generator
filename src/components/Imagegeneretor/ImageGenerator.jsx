150b4d67bb488ec60141331cae3ecda8c4f01f71
saket saurav <saketsingh6996@gmail.com>
2025-07-11 22:57:58 +0530

    gave style to search box and functionality

--- src/components/Imagegeneretor/ImageGenerator.jsx
+++ src/components/Imagegeneretor/ImageGenerator.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import './ImageGenerator.css';
 import default_img from '../../assets/default_img.svg';
 
 
 const ImageGenerator = () => {
+    const [image_url , setImage_url] =useState("/");
+    let inputRef =useRef(null);
+
+    const ImageGenerator =async () => {
+        if (inputRef.current.value==="") {
+            return 0;
+        }
+        const response = await fetch (
+            
+        )
+    }
 return(
     <div className="ai-image-generator">
         <div className="header">Ai image <span>generator</span></div>
         <div className="img-loading">
-            <div className="image"><img src={default_img} alt="deafult" /></div>
+            <div className="image"><img src={imgage_url==="/"?default_image:image_url} alt="deafult" /></div>
         </div>
         <div className="search-box">
-            <input type="text" className="search-input"placeholder="Desscribe what yoy want to see" />  
+            <input type="text" ref={inputRef} className="search-input"placeholder="Desscribe what yoy want to see" />  
             <div className="generate-btn">Generate</div> 
              </div>
     </div>
