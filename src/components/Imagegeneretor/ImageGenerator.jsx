5e4ad881e77154d6bb71f78f92fe54729fe2c661
Saket Singh <saketsingh6996@gmail.com>
2025-07-13 23:08:59 +0530

    style+ ,apikeys,fixed error

--- src/components/Imagegeneretor/ImageGenerator.jsx
+++ src/components/Imagegeneretor/ImageGenerator.jsx
@@ -12,8 +12,24 @@ const ImageGenerator = () => {
             return 0;
         }
         const response = await fetch (
+         "https://api.openai.com/v1/images/generations",
+            {
+                method :"POST",
+                headers:{
+                    "content-Type": "application/jason",
+                    Authorization:
+                    "Bearer "
+                    PASTE API KEY HERE  
+                    "User-Agent":"Chrome", 
+                },
+                body:JSON.stringify({
+                  prompt:'${inputRef.current.value}',
+                  n:1,
+                  size:"512x512",  
+                }),
+            }
             
-        )
+        );
     }
 return(
     <div className="ai-image-generator">
